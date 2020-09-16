0ee18bcb2504ae65e6ea9bbb9702808f0685c7da
jruiz181 <53583385+jruiz181@users.noreply.github.com>
2020-09-16 13:18:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+//step 1 select objects on screen & define globals//
+const holes = document.querySelectorAll('.hole');
+const scoreboard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const countdownBoard = document.querySelector('.countdown');
+const startButton = document.querySelector('.startButton');
+let lastHole;
+let timeUp = false;
+let score = 0;
+let countdown;
+let timeLimit = 2000
+function pickRandomHole(holes) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole){
+        return(pickRandomHole(holes));
+    }
+    lastHole = hole;
+    return hole;
+}
+//step 2 add class 'up'//
+function popOut(){
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){
+        hole.classList.remove('up');
+        if (!timeUp) {
+            popOut();
+        }
+    }, time);
+}
+//step 3 dynamically change text content to countdown//
+function startGame() {
+    countdown = timeLimit/20;
+    scoreboard.textContent = 0;
+    scoreboard.style.display = 'block';
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit);
+    let startCountdown = setInterval(function(){
+        countdown -= 1;
+        countdownBoard.textContent = countdown;
+        if (countdown < 0){
+            countdown = 0;
+            clearInterval(startCountdown)
+            countdownBoard.textContent = 'Times Up!! Thanks for assaulting tiny yodas.';
+        }
+    }, 1000)
+}
+//step 4 adding event listeners//
+startButton.addEventListener('click', startGame);
+
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("yoda2.png")';
+    this.style.pointerEvents = 'none';
+    setTimeout(() =>{
+        this.style.backgroundImage = 'url("yoda1.png")';
+        this.style.pointerEvents = 'all';
+    }, 800)
+    scoreboard.textContent = score;
+}
+//step 5 keep track of score with forEach//
+moles.forEach(mole => mole.addEventListener('click', whack));
